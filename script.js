b39b528c6042eae478fe6192aed18e5989a6d476
justin132006 <nahida132006@gmail.com>
2023-10-27 21:42:16 +0800

    new updated

---
+++ script.js
@@ -0,0 +1,41 @@
+let tablinks = document.querySelectorAll('.tab-links');
+let tabContents = document.querySelectorAll('.tab-contents');
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabContent of tabContents){
+        tabContent.classList.remove('active-tab');
+    }
+    event.currentTarget.classList.add('active-link');
+    document.getElementById(tabname).classList.add('active-tab');
+}
+// contact 
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwo-U7M5aQUOYGG1FGFgy4d9Pk1kgAHiutxhwg9TzGA9exTpcVIg3Fiz3oz1FJ9cE4Nhg/exec'
+        const form = document.forms['submit-to-google-sheet']
+        const msg = document.getElementById('Msg')
+      
+        form.addEventListener('submit', e => {
+          e.preventDefault()
+          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => {
+                msg.innerHTML = 'Message sent successfully Thank you!'
+                setTimeout(function(){
+                    msg.innerHTML = ''
+                },3000)
+                form.reset()
+            })
+            .catch(error => console.error('Error!', error.message))
+        })
+
+// menu
+let sidemenu = document.querySelector('#sidemenu');
+
+function openmenu(){
+    sidemenu.style.right = "0"
+}
+function closemenu(){
+    sidemenu.style.right = '-200px';
+}
---
+++ tip calculator/script.js
@@ -0,0 +1,64 @@
+let bill = document.querySelector('#bill');
+let tip = document.querySelector('#tip');
+let member = document.querySelector('#member');
+let btn = document.querySelector('#btn');
+let result = document.querySelector('#result');
+let resulttip = document.querySelector('#resulttip');
+let total = document.querySelector('#total');
+btn.addEventListener('click',calculate);
+function calculate(){
+   let pay = bill.value;
+   let hold;
+   let people = member.value;
+   let money;
+   if(pay == '' || people == ''){
+    resulttip.textContent = 'Tip:'
+    result.textContent = "Please enter the bill amount and number of member"
+   }
+   else if(tip.value == 25){
+    hold = pay * 0.25;
+    money = hold / people;
+    total.textContent = 'Total:'+ pay;
+    result.textContent = "Tip per person is " + money;
+    resulttip.textContent = 'Tip:' + hold;
+   }
+   else if(tip.value == 20){
+    hold = pay * 0.20;
+    money = hold / people;
+    total.textContent = 'Total:'+ pay;
+    result.textContent = "Tip per person is " + money;
+    resulttip.textContent = 'Tip:' + hold;
+   }
+   else if(tip.value == 15){
+    hold = pay * 0.15;
+    money = hold / people;
+    total.textContent = 'Total:'+ pay;
+    result.textContent = "Tip per person is " + money;
+    resulttip.textContent = 'Tip:' + hold;
+   }
+   else if(tip.value == 10){
+    hold = pay * 0.10;
+    money = hold / people;
+    total.textContent = 'Total:'+ pay;
+    result.textContent = "Tip per person is " + money;
+    resulttip.textContent = 'Tip:' + hold;
+   }
+   else if(tip.value == 5){
+    hold = pay * 0.05;
+    money = hold / people;
+    total.textContent = 'Total:'+ pay;
+    result.textContent = "Tip per person is " + money;
+    resulttip.textContent = 'Tip:' + hold;
+   }
+   else{
+
+   }
+}
+document.querySelector('#Reset').addEventListener('click', reset);
+function reset(){
+    total.textContent = 'Total:'
+    bill.value = ''
+    member.value = ''
+    result.textContent = 'Result'
+    resulttip.textContent = 'Tip:'
+}
